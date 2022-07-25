b12607448403761c7e334238b22071a88f0fea23
ts-develops <tawfiqrahman00@gmail.com>
2022-07-25 19:29:21 +0100

    nav bar created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+  return (
+    <div className='flex justify-end items-center bg-black h-20 w-full px-4 fixed'>
+        <ul className='hidden md:flex'>
+            <li className=' text-gray-500 px-4 cursor-pointer capitalize font-medium hover:scale-100 duration-200'> home</li>
+            <li className=' text-gray-500 px-4 cursor-pointer capitalize font-medium hover:scale-100 duration-200'> about me</li>
+            <li className=' text-gray-500 px-4 cursor-pointer capitalize font-medium hover:scale-100 duration-200'> Portfolio</li>
+            <li className=' text-gray-500 px-4 cursor-pointer capitalize font-medium hover:scale-100 duration-200'> contact us</li>
+        </ul>
+
+        <div onClick={() => setNav(!nav)} className='text-white pr-5 cursor-pointer z-10 md:hidden'>
+            {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
+        </div>
+
+        {nav &&  <ul className='md:hidden flex flex-col items-center justify-center top-0 left-0 w-full h-screen absolute bg-black text-gray-500'>
+        <li className='px-4 cursor-pointer capitalize py-6 text-4xl'> Home</li>
+        <li className='px-4 cursor-pointer capitalize py-6 text-4xl'> about me</li>
+        <li className='px-4 cursor-pointer capitalize py-6 text-4xl'> Portfolio</li>
+        <li className='px-4 cursor-pointer capitalize py-6 text-4xl'> Contact Us</li>
+        </ul>}
+
+       
+    </div>
+  )
+}
+
+export default Navbar
